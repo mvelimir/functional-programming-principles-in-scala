9c3f8b58836ff5f81d45637bf0ec9138615edb94
mvelimir <velimirmilinkovic1@gmail.com>
2021-05-07 17:01:20 +0200

    Reformat recfun

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -14,40 +14,34 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-
-  def pascal(c: Int, r: Int): Int = {
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
     if (c == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  }
 
   /**
-   * Exercise 2
-   */
-
+    * Exercise 2
+    */
   def balance(chars: List[Char]): Boolean = {
-
     @tailrec def bracketTotal(chars: List[Char], total: Int): Boolean = chars match {
-      case Nil => total == 0
+      case Nil      => total == 0
       case '(' :: t => bracketTotal(t, total + 1)
       case ')' :: t => if (total > 0) bracketTotal(t, total - 1) else false
-      case _ :: t => bracketTotal(t, total)
+      case _ :: t   => bracketTotal(t, total)
     }
 
     bracketTotal(chars, 0)
   }
 
   /**
-   * Exercise 3
-   */
-
+    * Exercise 3
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    @tailrec def countChangeHelper(money: Int, coins: List[Int], total: Int): Int = {
+    @tailrec def countChangeHelper(money: Int, coins: List[Int], total: Int): Int =
       if (money < 0 || coins.isEmpty || coins.head == 0) total
       else if (money == 0) total + 1
       else countChangeHelper(money, coins.tail, total + countChange(money - coins.head, coins))
-    }
 
     countChangeHelper(money, coins, 0)
 
